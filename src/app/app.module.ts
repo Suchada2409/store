6fc87edac91ad36db82b35e9a8f0a2b844ff8179
Train014 <Train014@MOJ.GO.TH>
2020-01-14 14:31:32 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,22 +10,31 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { CartComponent } from './cart/cart.component';
+import { from } from 'rxjs';
+import { ShippingComponent } from './shipping/shipping.component';
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path: 'products/:productsId' , component : ProductDetailsComponent}
+      {path: 'products/:productsId' , component : ProductDetailsComponent},
+      {path: 'cart' , component : CartComponent},
+      {path: 'shipping' , component : ShippingComponent}
     ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
